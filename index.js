71b69eaca20a55fe44a37a1a4edaa5573b5e53a1
patsicko <patsicko@g>
2022-07-18 11:30:13 +0200

    Featch API

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 let body=document.getElementById("body");
 
 
-    document.addEventListener("DOMContentLoaded",(e)=>{
-   
-        
-        fetchData();
+const data=()=>{
+ const items=fetch("https://api.github.com/users")
+ .then(res=>res.json());
+items.then(datum=>datum.map(item=>body.innerHTML+=`<img src=${item.avatar_url}>`))
 
-
-    })
-
-    function fetchData(){
-      
-
-      console.log("loading data....");
-      
-
-      const option={
-      
-        method:"GET"
-      }
-      
-      const request = new Request("https://api.github.com/users");
-
-      fetch(request).then(response=>response.json()).then(data=>console.log(data)).catch(error=>{
-        console.log(error);
-      });
-    }
+items.then(data=>console.log(data));
 
 
 
-  
+
+}
+ data();
+
+
+//  fetch("https://api.github.com/users")
+//  .then(res=>res.json())
+//  .then(json=>console.log(json));
+
+
+ fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((json) => json.map(item=>body.innerHTML+=`<img src=${item.image}>`) );
+//  
+
+
+
+// const loadData=async()=>{
+//  const data= await (await fetch("https://api.github.com/users")).json();
+
+//  console.log(data);
+// }
+
+// loadData();
